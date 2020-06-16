627320356fa6281074c1f90f42f16e1310f0993f
Markin Dessanti <markindessanti@gmail.com>
2020-06-16 15:12:18 -0300

    Finalizada a aula 15.02 Instalando e Usando Typescript

---
+++ index.ts
@@ -0,0 +1,8 @@
+function somar(numero1:number, numero2:number) {
+	return numero1 + numero2;
+}
+
+const numero1 = 10;
+const numero2 = 5;
+const resultado = somar(numero1, numero2);
+console.log(resultado);
